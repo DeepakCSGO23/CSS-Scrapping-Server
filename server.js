6a4213ec20367ca8d450705af1d2d0a25484a65e
sample github <kkvdhoorika@gmail.com>
2024-08-13 14:26:15 +0530

    Third Commit

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ require("dotenv").config();
 
 // Create a HTTP server
 const server=http.createServer((req,res)=>{
-    console.log('new client connected')
-    // if(req.method==='POST'){
-    //     let body='';
-    //     req.on('data',chunk=>{
-    //         // Conveting buffer to string
-    //         body+=chunk.toString()
-    //     })
-    //     req.on('end',()=>{
-    //         // Parsing the CSS data
-    //         const root=postcss.parse(body,{parser:safeParser})
-    //         console.log(root)
-    //     })
-    // }
+    if(req.method==='POST'){
+        console.log('parsing css data wait...')
+        // let body='';
+        // req.on('data',chunk=>{
+        //     // Conveting buffer to string
+        //     body+=chunk.toString()
+        // })
+        // req.on('end',()=>{
+        //     // Parsing the CSS data
+        //     const root=postcss.parse(body,{parser:safeParser})
+        //     console.log(root)
+        // })
+    }
 })
 
 server.listen(process.env.PORT,(err)=>{
