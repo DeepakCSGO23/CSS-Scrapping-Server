5cac0bcc0bda0cd65338d0e82ec1dfabff8afd48
sample github <kkvdhoorika@gmail.com>
2024-08-20 12:17:29 +0530

    hhhhh

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ const server = http.createServer((req, res) => {
                     // 
                     rule.walkDecls(decl => {
                         // is a css variable name
-                        if(decl.prop.startsWith('--')&&(decl.value.includes('#')||decl.value.includes('rgba')||decl.value.includes('rgb')||decl.value.includes('hsl'))){
+                        // Getting the root css variable name and its value
+                        if(decl.prop.startsWith('--')&&!decl.value.includes('var(')&&(decl.value.includes('#')||decl.value.includes('rgba')||decl.value.includes('rgb')||decl.value.includes('hsl'))){
                             cssVariables.set(decl.prop,decl.value)
                         }
                         // Check if the declaration is for color
